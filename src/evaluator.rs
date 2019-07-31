20acf2686cb738b427db746ad42b961d9f3f086d
kei-s <kei.shiratsuchi@gmail.com>
2019-08-01 08:22:49 +0900

    Eval infix expression
    [日記]
    - 本の通り

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -41,6 +41,7 @@ impl_eval!(Expression, self, {
         Expression::IntegerLiteral(exp) => exp.eval(),
         Expression::Boolean(exp) => exp.eval(),
         Expression::PrefixExpression(exp) => exp.eval(),
+        Expression::InfixExpression(exp) => exp.eval(),
         _ => panic!(),
     }
 });
@@ -87,6 +88,39 @@ fn eval_minus_prefix_operator_expression(right: Object) -> Object {
     }
 }
 
+impl_eval!(InfixExpression, self, {
+    let left = self.left.eval();
+    let right = self.right.eval();
+    eval_infix_expression(&self.operator, left, right)
+});
+
+fn eval_infix_expression(operator: &String, left: Object, right: Object) -> Object {
+    if let Object::Integer(l) = left {
+        if let Object::Integer(r) = right {
+            return eval_integer_infix_expression(operator, l, r);
+        }
+    }
+    return match operator.as_str() {
+        "==" => native_bool_to_boolean_object(left == right),
+        "!=" => native_bool_to_boolean_object(left != right),
+        _ => NULL
+    }
+}
+
+fn eval_integer_infix_expression(operator: &String, left_val: i64, right_val: i64) -> Object {
+    match operator.as_str() {
+        "+" => Object::Integer(left_val + right_val),
+        "-" => Object::Integer(left_val - right_val),
+        "*" => Object::Integer(left_val * right_val),
+        "/" => Object::Integer(left_val / right_val),
+        "<" => native_bool_to_boolean_object(left_val < right_val),
+        ">" => native_bool_to_boolean_object(left_val > right_val),
+        "==" => native_bool_to_boolean_object(left_val == right_val),
+        "!=" => native_bool_to_boolean_object(left_val != right_val),
+        _ => NULL,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::super::lexer::Lexer;
@@ -96,7 +130,23 @@ mod tests {
 
     #[test]
     fn test_eval_integer_expression() {
-        let tests = vec![("5", 5), ("10", 10), ("-5", -5), ("-10", -10)];
+        let tests = vec![
+            ("5", 5),
+            ("10", 10),
+            ("-5", -5),
+            ("-10", -10),
+            ("5 + 5 + 5 + 5 - 10", 10),
+            ("2 * 2 * 2 * 2 * 2", 32),
+            ("-50 + 100 + -50", 0),
+            ("5 * 2 + 10", 20),
+            ("5 + 2 * 10", 25),
+            ("20 + 2 * -10", 0),
+            ("50 / 2 * 2 + 10", 60),
+            ("2 * (5 + 10)", 30),
+            ("3 * 3 * 3 + 10", 37),
+            ("3 * (3 * 3) + 10", 37),
+            ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
+        ];
 
         for tt in tests {
             let input = tt.0.to_string();
@@ -108,7 +158,18 @@ mod tests {
 
     #[test]
     fn test_eval_boolean_expression() {
-        let tests = vec![("true", true), ("false", false)];
+        let tests = vec![
+            ("true", true),
+            ("false", false),
+            ("1 < 2", true),
+            ("1 > 2", false),
+            ("1 < 1", false),
+            ("1 > 1", false),
+            ("1 == 1", true),
+            ("1 != 1", false),
+            ("1 == 2", false),
+            ("1 != 2", true),
+        ];
 
         for tt in tests {
             let input = tt.0.to_string();
@@ -127,6 +188,15 @@ mod tests {
             ("!!true", true),
             ("!!false", false),
             ("!!5", true),
+            ("true == true", true),
+            ("false == false", true),
+            ("true == false", false),
+            ("true != false", true),
+            ("false != true", true),
+            ("(1 < 2) == true", true),
+            ("(1 < 2) == false", false),
+            ("(1 > 2) == true", false),
+            ("(1 > 2) == false", true),
         ];
 
         for tt in tests {
